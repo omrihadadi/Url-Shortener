da473102356ce03ec365e880b9bd78bdcc044f2d
omrihadadi <omrihadadi47@gmail.com>
2024-06-23 14:48:37 +0300

    Check the CI workflow #6

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,14 +2,14 @@ const Url = require('../models/urlModel');
 const { generateShortUrl } = require('../utils/urlUtils');
 
 const createShortUrl = async (req, res) => {
-  const { originalUrl } = req.body;
+  const { originalUrl } = req.body; 
 
   try {
     const shortUrl = generateShortUrl();
     console.log(shortUrl);
     const newUrl = await Url.create({ originalUrl, shortUrl });
     res.status(201).json(newUrl);
-  }  catch (error) {
+  }   catch (error) {
     res.status(500).json({ error: 'Failed to create short URL' });
   }
 };
