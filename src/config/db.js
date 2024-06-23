b73fef48389adba7568e657ebbbc1faab11fa0e7
omrihadadi <omrihadadi47@gmail.com>
2024-06-23 16:34:59 +0300

    Update db.js file

--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,6 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  dialectOptions: {
-    // Ensure this matches the MySQL server's charset
-    charset: 'utf8mb4',
-    // This enables support for emoji in the database
-    collate: 'utf8mb4_unicode_ci',
-    // Support for multiple statements in one query (e.g., batch inserts)
-    multipleStatements: true,
-  },
 });
 
 module.exports = sequelize;
